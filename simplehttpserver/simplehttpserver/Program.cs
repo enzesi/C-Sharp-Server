95a7ecaf39fff9ddff56f5ef4d614f7273cf25b6
enzesi <32287882+enzesi@users.noreply.github.com>
2018-11-10 17:40:50 -0500

    server

---
+++ simplehttpserver/simplehttpserver/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace WebTestServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebServer ws = new WebServer("http://localhost:8080/");
+            ws.Run();
+            Console.WriteLine("A simple webserver. Press a key to quit.");
+            Console.ReadKey();
+            ws.Stop();
+        }
+    }
+}
